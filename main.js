2bcf4594e8828e02e9501678e31f4ba7fce6d6af
am1k <vitaliy_bog@mail.ru>
2015-08-13 15:22:24 +0300

    initial

--- main.js
+++ main.js
@@ -35,39 +35,72 @@
 
 createArray = function(){
     var arrayPage = [];
-    var items = 50;
+    var items = numPages();
     for( var i = 0; i < items; i++){
         arrayPage.push(i+1);
     }
     return arrayPage;
 };
 
-var content = document.querySelector('.select-template');
 
-content.innerHTML = tmpl('optionsSelect', {
-    items: ['red', 'blue', 'red', 'yellow', 'black'],
-    selected: 'yellow'
-});
+var information = document.querySelector('.information');
+var recordsPerPage = 2;
+var elemArray = [
+    'one',
+    'two',
+    'three',
+    'four',
+    'five',
+    'six',
+    'seven',
+    'eight',
+    'nine',
+    'ten',
+    'eleven',
+    'twelve',
+    'thirteen',
+    'fourteen',
+    'fifteen',
+    'sixteen',
+    'seventeen'
+];
+
+numPages = function(){
+    return Math.ceil(elemArray.length / recordsPerPage);
+};
+
+changePage = function(page){
+    var elements = [];
+    var end =  Math.min((page * recordsPerPage), elemArray.length);
+    console.log(end);
+    for( var i = (page - 1) * recordsPerPage; i < end; i++){
+        elements.push(elemArray[i]);
+    }
+    information.innerHTML = tmpl('info', {
+        elements: elements
+    });
+};
+
+changePage(1);
 
 var results = document.querySelector('.pagination');
 
 render = function(newPage){
-    newPage = newPage || 2;
+    newPage = newPage || 1;
     itemsLength = createArray();
-    console.log(newPage);
     results.innerHTML = tmpl('paginationTemplate', {
         items: itemsLength,
         currentPage: newPage,
         start: Math.max(newPage - 2, 1),
-        end: Math.min(newPage + 1, itemsLength.length-1)
+        end: Math.min(newPage + 1, itemsLength.length - 1)
     });
 };
-
 render();
 
 results.addEventListener('click',function(e){
     if(e.srcElement.tagName.toLocaleLowerCase() === 'a'){
         render(parseInt(e.srcElement.innerHTML, 10));
+        changePage(parseInt(e.srcElement.innerHTML, 10));
     }
 });
 
